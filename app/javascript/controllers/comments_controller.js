e3a11c4afc3545c1aa701fe898c9b9adeee04ce4
jacob jemal <jacobjemal7@gmail.com>
2022-09-29 14:18:52 -0400

    comments form can toggle witht the edit btn

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -1,7 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    this.element.textContent = "Hello World!"
+    initialize() {
+    }
+    connect() {
+  }
+    toggleForm(event) {
+    console.log("i clicked the edit btn")
+    event.preventDefault()
+    event.stopPropagation()
+    const formID = event.params["form"]
+    const commentBodyID = event.params["body"]
+    const form = document.getElementById(formID)
+    form.classList.toggle("d-none")
+    const commentBody = document.getElementById(commentBodyID)
+    commentBody.classList.toggle("d-none")
   }
 }
