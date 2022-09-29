e8656f9d84fd1cb5bcec52058ebb8c1e27c2834a
jacob jemal <jacobjemal7@gmail.com>
2022-09-29 12:33:48 -0400

    added an updated_at in time ago

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(email: "jacob@example.com", password: "password", password_confirmation: "password" )
+User.create(email: "jacob@example.com", name: "jacob", password: "password", password_confirmation: "password" )
+User.create(email: "jhon@example.com", name: "jhon", password: "password", password_confirmation: "password" )
 
 10.times do |x|
     Post.create(title: "title #{x}", body: "body #{x} words go here", user_id: User.first.id)
