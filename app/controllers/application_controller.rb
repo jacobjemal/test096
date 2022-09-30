4dd4dfff0d80da1b551ac7933300d2f84fd1568a
jacob jemal <jacobjemal7@gmail.com>
2022-09-30 15:56:46 -0400

    notifaction partal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_notifications, if: :current_user
+
+  private
+
+  def set_notifications
+    notifications = Notification.where(recipient: current_user).newst_first.limit(9)
+    @unread = notification.unread
+    @read = notification.read
+  end
 end
