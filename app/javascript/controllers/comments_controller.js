0a5b6cc23f47ff906694cbd15391971c904ceede
jacob jemal <jacobjemal7@gmail.com>
2022-09-30 17:19:30 -0400

    added cancle btn to edit btn

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -11,10 +11,29 @@ export default class extends Controller {
     event.stopPropagation()
     const formID = event.params["form"]
     const commentBodyID = event.params["body"]
+    const editButtonID = event.params["edit"]
+
     const form = document.getElementById(formID)
     form.classList.toggle("d-none")
     form.classList.toggle("mt-5")
     const commentBody = document.getElementById(commentBodyID)
     commentBody.classList.toggle("d-none")
+    const editButton = document.getElementById(editButtonID);
+    this.toggleEditButton(editButton);
+
+  }
+  toggleEditButton(editButton) {
+    if (editButton.innerText === "Edit") {
+      editButton.innerText = "Cancel";
+      this.toggleEditButtonClass(editButton);
+    } else {
+      editButton.innerText = "Edit";
+      this.toggleEditButtonClass(editButton);
+    }
+  }
+
+  toggleEditButtonClass(editButton) {
+    editButton.classList.toggle("btn-secondary");
+    editButton.classList.toggle("btn-warning");
   }
 }
